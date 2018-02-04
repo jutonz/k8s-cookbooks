5d824687fb0e41fc1fe34b6cec89be554d69e339
Justin Toniazzo <jutonz42@gmail.com>
2018-02-03 21:02:35 -0500

    wip

--- recipes/setup-master.rb
+++ recipes/setup-master.rb
@@ -4,7 +4,6 @@
 SITE = "k8s-master.jutonz.com"
 
 include_recipe "apt::default"
-include_recipe "acme"
 
 group "ubuntu" do
   action :create
@@ -43,46 +42,15 @@ apt_package "docker-engine"
   kubelet
   kubectl
   kubernetes-cni
+  kubeadm
 ).each { |pkg| apt_package(pkg) }
-
-# Install kubeadm, which automates k8s setup
-# Optionally skip this and setup k8s manually
-# See https://kubernetes.io/docs/getting-started-guides/kubeadm/
-apt_package "kubeadm"
-
-# Install nginx so we can respond to http cert verification
-apt_package "nginx"
-service "nginx" do
-  action :nothing
-  supports %i(enable disable start stop restart reload status)
-end
-
-template "/etc/nginx/sites-available/default" do
-  source "default-site-available.erb"
-  variables({
-    site: SITE
-  })
-  notifies :start, "service[nginx]", :immediately
-end
-
-directory "/etc/ssl/mycerts"
-
-# Setup letsencrypt certs for https
-acme_certificate SITE do
-  wwwroot "/var/www/html"
-  crt "/etc/ssl/mycerts/#{SITE}.crt"
-  chain "/etc/ssl/mycerts/#{SITE}-chain.crt"
-  key "/etc/ssl/mycerts/#{SITE}.key"
-  not_if { ::File.exists?("/etc/ssl/mycerts/#{SITE}.crt") }
-  notifies :stop, "service[nginx]", :immediately
-end
 
 execute "kubeadm reset"
 
 token = data_bag_item("secrets", "kubeadm_token")["key"]
 execute "kubeadm init --token #{token}" do
   user "root"
-  not_if { ::File.exist?("/etc/kubernetes/admin.conf") }
+  not_if { ::File.exists?("/etc/kubernetes/admin.conf") }
 end
 
 execute "cp /etc/kubernetes/admin.conf /home/ubuntu/admin.conf"
@@ -109,11 +77,11 @@ end
 
 # Store tls keys in a secret
 # TODO: make this its own recipe so we can update secret as tls certs rotate?
-execute "KUBECONFIG=/home/ubuntu/admin.conf kubectl create secret tls tls-secret --key /etc/ssl/mycerts/#{SITE}.key --cert /etc/ssl/mycerts/#{SITE}.crt"
+#execute "KUBECONFIG=/home/ubuntu/admin.conf kubectl create secret tls tls-secret --key /etc/ssl/mycerts/#{SITE}.key --cert /etc/ssl/mycerts/#{SITE}.crt"
 
-execute "KUBECONFIG=/home/ubuntu/admin.conf kubectl taint nodes --all node-role.kubernetes.io/master-" do
-  user "ubuntu"
-end
+#execute "KUBECONFIG=/home/ubuntu/admin.conf kubectl taint nodes --all node-role.kubernetes.io/master-" do
+  #user "ubuntu"
+#end
 
 # Set a motd explaining how to join nodes to the cluster
 template "/etc/motd" do
